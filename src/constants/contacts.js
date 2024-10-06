0dd28d72730196cc7d28c26e0c3aef2ad8980017
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-10-06 17:55:41 +0200

    process_step_004_a

--- src/constants/contacts.js
+++
@@ -1,9 +0,0 @@
-// const path = require('path');
-
-import { join } from 'path';
-
-// export const PATH_DB = path.join(__dirname, '../db/db.json');
-
-export const PATH_DB = join(process.cwd(), 'src', 'db', 'db.json');
-
-// module.exports = { PATH_DB };
--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,8 +1,6 @@
-// src/db/models/student.js
-
 import { model, Schema } from 'mongoose';
 
-const studentsSchema = new Schema(
+const contactsSchema = new Schema(
   {
     name: {
       type: String,
@@ -33,4 +31,4 @@ const studentsSchema = new Schema(
   },
 );
 
-export const StudentsCollection = model('students', studentsSchema);
+export const ContactsCollection = model('contacts', contactsSchema);
