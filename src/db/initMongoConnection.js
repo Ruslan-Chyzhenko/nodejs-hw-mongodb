d93b6b40cc192cd090320eab7ef85dbfa4174d79
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-10-06 22:42:42 +0200

    process_step_005_006

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,32 +1,32 @@
 import mongoose from 'mongoose';
-import { ENV_VARS } from '../constants/index.js';
+// import { ENV_VARS } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
 export const initMongoDB = async () => {
-  const connectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
-    ENV_VARS.MONGODB_PASSWORD,
-  )}@${env(ENV_VARS.MONGODB_URL)}/${env(
-    ENV_VARS.MONGODB_DB,
-  )}?retryWrites=true&w=majority&appName=Cluster0`;
+  //   const connectionLink = `mongodb+srv://${env(ENV_VARS.MONGODB_USER)}:${env(
+  //     ENV_VARS.MONGODB_PASSWORD,
+  //   )}@${env(ENV_VARS.MONGODB_URL)}/${env(
+  //     ENV_VARS.MONGODB_DB,
+  //   )}?retryWrites=true&w=majority&appName=Cluster0`;
 
-  try {
-    await mongoose.connect(connectionLink);
-    console.log('Successfully established database connection!');
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-  //    try {
-  //     const user = env('MONGODB_USER');
-  //     const pwd = env('MONGODB_PASSWORD');
-  //     const url = env('MONGODB_URL');
-  //     const db = env('MONGODB_DB');
-  //     await mongoose.connect(
-  //       `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
-  //     );
-  //     console.log('Mongo connection successfully established!');
+  //   try {
+  //     await mongoose.connect(connectionLink);
+  //     console.log('Successfully established database connection!');
   //   } catch (err) {
-  //     console.log('Error while setting up mongo connection', err);
+  //     console.log(err);
   //     throw err;
   //   }
+  try {
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (err) {
+    console.log('Error while setting up mongo connection', err);
+    throw err;
+  }
 };
