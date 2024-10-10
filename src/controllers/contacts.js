fbe05172d82aeae7da1a1a47f6eb36886edb1ff9
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-10-10 22:45:33 +0200

    process_step_01_to 03

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,9 @@
 // src/controllers/contacts.js
 import { getAllContacts, getContactById } from '../services/contacts';
 import createHttpError from 'http-errors';
+import { createContact } from '../services/contacts';
+import { deleteContact } from '../services/contacts';
+import { updateContact } from '../services/contacts';
 
 export const getContactsController = async (req, res, next) => {
   try {
@@ -37,5 +40,63 @@ export const getContactByIdController = async (req, res, next) => {
     status: 200,
     message: `Succefuly found contact with id ${contactId}`,
     data: contact,
+  });
+};
+
+export const createContactController = async (req, res) => {
+  const contact = await createContact(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: `Successfully created a student!`,
+    data: contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const contact = await deleteContact(contactId);
+
+  if (!contact) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const upsertContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a contact!`,
+    data: result.contact,
+  });
+};
+
+export const patchContactController = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await updateContact(contactId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
   });
 };
