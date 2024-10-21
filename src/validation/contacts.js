2c541bb798e928c7a0953f5d50a425568511a629
Ruslan-Chyzhenko <144059463+Ruslan-Chyzhenko@users.noreply.github.com>
2024-10-21 21:55:52 +0200

    finish_01

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,22 +3,48 @@
 import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  age: Joi.number().integer().min(6).max(16).required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
-  avgMark: Joi.number().min(2).max(12).required(),
-  onDuty: Joi.boolean(),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string',
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+    'any.required': 'Username is required',
+  }),
+  phoneNumber: Joi.string().required().messages({
+    'any.required': 'Phone Number is required',
+  }),
+  email: Joi.string().email(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({ 'any.required': 'Contact Type is required' }),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.string(),
   email: Joi.string().email(),
-  age: Joi.number().integer().min(6).max(16),
-  gender: Joi.string().valid('male', 'female', 'other'),
-  avgMark: Joi.number().min(2).max(12),
-  onDuty: Joi.boolean(),
+  isFavorite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
+
+// export const createContactSchema = Joi.object({
+//   name: Joi.string().min(3).max(20).required(),
+//   email: Joi.string().email().required(),
+//   age: Joi.number().integer().min(6).max(16).required(),
+//   gender: Joi.string().valid('male', 'female', 'other').required(),
+//   avgMark: Joi.number().min(2).max(12).required(),
+//   onDuty: Joi.boolean(),
+// });
+
+// export const updateContactSchema = Joi.object({
+//   name: Joi.string().min(3).max(20),
+//   email: Joi.string().email(),
+//   age: Joi.number().integer().min(6).max(16),
+//   gender: Joi.string().valid('male', 'female', 'other'),
+//   avgMark: Joi.number().min(2).max(12),
+//   onDuty: Joi.boolean(),
+// });
 
 // const dataToValidate = {
 //   name: 'John Doe',
@@ -35,21 +61,6 @@ export const updateContactSchema = Joi.object({
 //   console.log('Data is valid!');
 // }
 
-// Оголошення схеми з кастомізованими повідомленнями
-// const createStudentSchema = Joi.object({
-//   name: Joi.string().min(3).max(30).required().messages({
-//     'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
-//     'string.min': 'Username should have at least {#limit} characters',
-//     'string.max': 'Username should have at most {#limit} characters',
-//     'any.required': 'Username is required',
-//   }),
-//   email: Joi.string().email().required(),
-//   age: Joi.number().integer().min(6).max(16).required(),
-//   gender: Joi.string().valid('male', 'female', 'other').required(),
-//   avgMark: Joi.number().min(2).max(12).required(),
-//   onDuty: Joi.boolean(),
-// });
-
-// const validationResult = createStudentSchema.validate(userData, {
+// const validationResult = createContactSchema.validate(userData, {
 //   abortEarly: false,
 // });
